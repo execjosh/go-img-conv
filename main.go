daec48001db2c3cee5fb22328172032844e90512
execjosh <buildjosh@gmail.com>
2018-04-18 00:58:09 +0900

    Refactor JPEG to PNG conversion into function

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"image"
 	"image/png"
@@ -13,36 +14,39 @@ import (
 
 func main() {
 	inputFilePath := os.Args[1]
-	fmt.Println(inputFilePath)
 
+	err := convertFromJpegToPng(inputFilePath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func convertFromJpegToPng(inputFilePath string) error {
 	f, err := os.Open(inputFilePath)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Cannot open file:", inputFilePath)
-		os.Exit(1)
+		return errors.New(fmt.Sprint("Cannot open file:", inputFilePath))
 	}
 	defer f.Close()
 
 	m, fmtName, err := image.Decode(f)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Cannot decode image:", inputFilePath)
-		os.Exit(1)
+		return errors.New(fmt.Sprint("Cannot decode image:", inputFilePath))
 	}
 	if fmtName != "jpeg" {
-		fmt.Fprintln(os.Stderr, "Expected JPEG but got:", fmtName)
-		os.Exit(1)
+		return errors.New(fmt.Sprint("Expected JPEG but got:", fmtName))
 	}
 
 	outputFilePath := strings.TrimSuffix(inputFilePath, filepath.Ext(inputFilePath)) + ".png"
-	fmt.Println(outputFilePath)
 	o, err := os.OpenFile(outputFilePath, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Cannot open for writing:", outputFilePath)
-		os.Exit(1)
+		return errors.New(fmt.Sprint("Cannot open for writing:", outputFilePath))
 	}
 	defer o.Close()
 
 	if err := png.Encode(o, m); err != nil {
-		fmt.Fprintln(os.Stderr, "Error converting to PNG:", err)
-		os.Exit(1)
+		return errors.New(fmt.Sprint("Error converting to PNG:", err))
 	}
+
+	return nil
 }
