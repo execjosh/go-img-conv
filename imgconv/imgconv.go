5af9288393553e8ba01c039645af984c72590805
execjosh <buildjosh@gmail.com>
2018-04-18 01:48:03 +0900

    Move traversal and conversion to own package

---
+++ imgconv/imgconv.go
@@ -0,0 +1,83 @@
+// Package imgconv implements image conversion convenience methods.
+package imgconv
+
+import (
+	"errors"
+	"fmt"
+	"image"
+	"image/png"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	_ "image/jpeg" // Register JPEG codec
+)
+
+// ConvertAllJpegToPngIn traverses inputDir and attempts to convert JPEGs to PNGs
+func ConvertAllJpegToPngIn(inputDir string) error {
+	return filepath.Walk(inputDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Fprintln(os.Stderr, path, ":", err)
+			return nil
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		if err := convertFromJpegToPng(path); err != nil {
+			fmt.Fprintln(os.Stderr, path, ":", err)
+		}
+
+		return nil
+	})
+}
+
+func detectContentType(f *os.File) string {
+	// Make sure we return to top of file
+	defer f.Seek(0, 0)
+
+	buf := make([]byte, 128)
+
+	n, err := f.Read(buf)
+	if err != nil && err != io.EOF {
+		return "unknown"
+	}
+
+	return http.DetectContentType(buf[:n])
+}
+
+func convertFromJpegToPng(inputFilePath string) error {
+	f, err := os.Open(inputFilePath)
+	if err != nil {
+		return errors.New(fmt.Sprint("Cannot open file"))
+	}
+	defer f.Close()
+
+	if detectContentType(f) != "image/jpeg" {
+		return errors.New("Not a JPEG")
+	}
+
+	m, fmtName, err := image.Decode(f)
+	if err != nil {
+		return errors.New(fmt.Sprint("Cannot decode image"))
+	}
+	if fmtName != "jpeg" {
+		return errors.New("Not a JPEG")
+	}
+
+	outputFilePath := strings.TrimSuffix(inputFilePath, filepath.Ext(inputFilePath)) + ".png"
+	o, err := os.OpenFile(outputFilePath, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return errors.New(fmt.Sprint("Cannot open output for writing: ", outputFilePath))
+	}
+	defer o.Close()
+
+	if err := png.Encode(o, m); err != nil {
+		return errors.New(fmt.Sprint("Error converting to PNG: ", err))
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,90 +1,17 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"image"
-	"image/png"
-	"io"
-	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
 
-	_ "image/jpeg"
+	"github.com/execjosh/go-img-conv/imgconv"
 )
 
 func main() {
 	inputDir := os.Args[1]
 
-	if err := convertAllJpegToPngIn(inputDir); err != nil {
+	if err := imgconv.ConvertAllJpegToPngIn(inputDir); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-}
-
-func convertAllJpegToPngIn(inputDir string) error {
-	return filepath.Walk(inputDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			fmt.Fprintln(os.Stderr, path, ":", err)
-			return nil
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		if err := convertFromJpegToPng(path); err != nil {
-			fmt.Fprintln(os.Stderr, path, ":", err)
-		}
-
-		return nil
-	})
-}
-
-func detectContentType(f *os.File) string {
-	// Make sure we return to top of file
-	defer f.Seek(0, 0)
-
-	buf := make([]byte, 128)
-
-	n, err := f.Read(buf)
-	if err != nil && err != io.EOF {
-		return "unknown"
-	}
-
-	return http.DetectContentType(buf[:n])
-}
-
-func convertFromJpegToPng(inputFilePath string) error {
-	f, err := os.Open(inputFilePath)
-	if err != nil {
-		return errors.New(fmt.Sprint("Cannot open file"))
-	}
-	defer f.Close()
-
-	if detectContentType(f) != "image/jpeg" {
-		return errors.New("Not a JPEG")
-	}
-
-	m, fmtName, err := image.Decode(f)
-	if err != nil {
-		return errors.New(fmt.Sprint("Cannot decode image"))
-	}
-	if fmtName != "jpeg" {
-		return errors.New("Not a JPEG")
-	}
-
-	outputFilePath := strings.TrimSuffix(inputFilePath, filepath.Ext(inputFilePath)) + ".png"
-	o, err := os.OpenFile(outputFilePath, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return errors.New(fmt.Sprint("Cannot open output for writing: ", outputFilePath))
-	}
-	defer o.Close()
-
-	if err := png.Encode(o, m); err != nil {
-		return errors.New(fmt.Sprint("Error converting to PNG: ", err))
-	}
-
-	return nil
 }
